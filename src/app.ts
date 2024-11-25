f2dcdf35a0a32a9ce14ab20b70041c63ab6b32e1
asm-dev <salamanca.m@outlook.com>
2024-11-25 15:18:06 +0000

    mock data on app to emulate expected app behaviour

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import { WeatherStatus } from "./weather/model";
 import { Weather } from "./weather/weather";
 
-const todayWeather = new Weather({
+const TODAY_WEATHER_MOCK = new Weather({
   temperature: {
     minTemperature: 15,
     maxTemperature: 30,
@@ -10,4 +10,26 @@ const todayWeather = new Weather({
   windSpeed: 20,
 });
 
-todayWeather.getAverageTemperature();
+const YESTERDAY_WEATHER_MOCK: Weather = new Weather({
+  temperature: {
+    minTemperature: 5,
+    maxTemperature: 10,
+  },
+  weather: WeatherStatus.SUNNY,
+  windSpeed: 20,
+});
+
+const WEATHER_DATA_MOCK: Weather[] = [
+  TODAY_WEATHER_MOCK,
+  YESTERDAY_WEATHER_MOCK,
+];
+
+const dailyAverage: number = Weather.getAverageDailyTemperature(
+  TODAY_WEATHER_MOCK.temperature
+);
+const totalAverage: number = Weather.getAverageTemperatureFromList(
+  WEATHER_DATA_MOCK.map((dailyData) => dailyData.temperature)
+);
+
+console.log("Media diaria del día:", dailyAverage);
+console.log("Media del total de días:", totalAverage);
